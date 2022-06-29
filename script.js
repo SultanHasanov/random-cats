c7c93a70e80a421eab9e20a4e1989047d9e95184
sultan <sultanhasanov987@gmail.com>
2022-06-29 11:27:05 +0300

    Добавил сетчик

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
 const url = "https://aws.random.cat/meow";
 const button = document.querySelector('.btn');
 const image = document.querySelector('img')
-
+const total = document.getElementById('total')
+let i = 0;
 
 async function fenchHandler () {
 try {
 const response = await fetch(url);
 const data = await response.json();
 image.src = data.file;
+total.textContent = i
 } catch (error){
     console.log(error);
 }
 }
 
 button.addEventListener('click', () => {
+i++
     fenchHandler()
 })
