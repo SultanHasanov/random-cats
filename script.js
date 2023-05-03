caf8755d5d1536655d5f96216a9fbc101be96bd5
sultan <sultanhasanov987@gmail.com>
2023-05-03 23:32:03 +0300

    Изменил апи

--- script.js
+++ script.js
@@ -1,21 +1,19 @@
-const url = "https://aws.random.cat/meow";
-const button = document.querySelector('.btn');
-const image = document.querySelector('img')
-const total = document.getElementById('total')
+const button = document.querySelector(".btn");
+const image = document.querySelector("img");
+const total = document.getElementById("total");
 let i = 0;
 
-async function fenchHandler () {
-try {
-const response = await fetch(url);
-const data = await response.json();
-image.src = data.file;
-total.textContent = i
-} catch (error){
-    console.log(error);
-}
+
+async function fenchHandler() {
+  const response = await fetch("https://api.thecatapi.com/v1/images/search")
+  const data = await response.json();
+  console.log(data[0].url);
+  image.src = data[0].url;
+  total.textContent = i;
+  
 }
 
-button.addEventListener('click', () => {
-i++
-    fenchHandler()
-})
+button.addEventListener("click", () => {
+  i++;
+  fenchHandler();
+});
