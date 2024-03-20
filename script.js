994ac579c16307a9f2dca3a63b752009a27df502
SultanHasanov <105391964+SultanHasanov@users.noreply.github.com>
2024-03-20 23:51:02 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,22 +2,11 @@ const button = document.querySelector(".btn");
 const image = document.querySelector("img");
 const total = document.getElementById("total");
 let i = 0;
-let timeLeft = 0;
-var timerDiv = document.getElementById("timer");
 
-function updateTimer() {
-  timerDiv.innerHTML = `Осталось времени: <span class="timer-value">${timeLeft}</span> сек.`;
-  if (timeLeft === 0) {
-    clearInterval(interval);
-    button.disabled = false;
-    button.classList.add("button-active");
-    timerDiv.innerHTML = "Можно нажимать";
-  } else {
-    timeLeft -= 1;
-  }
-}
 
-var interval = setInterval(updateTimer, 1000);
+
+
+
 async function fenchHandler() {
   const response = await fetch("https://api.thecatapi.com/v1/images/search");
   const data = await response.json();
